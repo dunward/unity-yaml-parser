a7b1fe978738a035743cdadad208fd5288e67fe0
Elik Song <elik@oxygen.ist>
2024-02-28 21:10:37 +0900

    Edit test code

--- test/index.ts
+++ test/index.ts
@@ -1,20 +1,19 @@
-import { UnityYamlParser } from "../main";
+import * as UnityYamlParser from "../main";
 
 const filePath: string = 'test/Demo.unity';
-var parser = new UnityYamlParser(filePath);
-parser.getYamlDataList().forEach((item) => {
+UnityYamlParser.parse(filePath).forEach((item) => {
     switch (item.classId) {
         case "1":
-            // console.log(`GameObject ${item.data.GameObject.m_Name}`);
+            // console.log(`GameObject ${JSON.stringify(item.data.GameObject.m_Component)}`);
             break;
         case "4":
-            console.log(`Transform ${item.data.Transform.m_Father.fileID}`);
+            console.log(`Transform ${item.data.Transform.m_GameObject.fileID}`);
             break;
         case "114":
             // console.log(`MonoBehaviour ${item.data.MonoBehaviour.m_Enabled}`);
             break;
         case "224":
-            console.log(`RectTransform ${item.data.RectTransform?.m_Father?.fileID ?? -1}`);
+            console.log(`RectTransform ${item.data.RectTransform.m_GameObject?.fileID ?? -1}`);
             break;
     }
 });
