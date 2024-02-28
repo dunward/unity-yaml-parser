527d892ab0245ca454b0a38a9e493c146ca24613
Elik Song <elik@oxygen.ist>
2024-02-28 21:10:30 +0900

    Edit parser structure

--- main.ts
+++ main.ts
@@ -2,31 +2,25 @@ import * as fs from 'fs';
 import * as yaml from 'yaml';
 import * as UnityClassType from './unityType/unityClassType';
 
-export class UnityYamlParser {
-    private splitData: string[] = [];
 
-    constructor(path: string) {
-        var data = fs.readFileSync(path, 'utf8');
-        this.splitData = data.split('---');
-    }
-
-    getYamlDataList(): UnityYamlData[] {
-        var result: UnityYamlData[] = [];
-        this.splitData.forEach((item) => {
-            var regex = /!u!(.*) &(.*)( stripped)?/;
-            var matching = item.match(regex);
-            if (matching != null) {
-                var classId = matching[1];
-                var fileId = matching[2];
-                var unityClass: UnityClassType.UnityClass = yaml.parse(item.replace(matching[0], ""));
-                result.push(new UnityYamlData(classId, fileId, unityClass, matching[3] != undefined));
-            }
-        });
-        return result;
-    }
+export function parse(path: string): UnityYamlData[] {
+    var data = fs.readFileSync(path, 'utf8');
+    const splitData = data.split('---');
+    var result: UnityYamlData[] = [];
+    splitData.forEach((item) => {
+        var regex = /!u!(.*) &(.*)( stripped)?/;
+        var matching = item.match(regex);
+        if (matching != null) {
+            var classId = matching[1];
+            var fileId = matching[2];
+            var unityClass: UnityClassType.UnityClass = yaml.parse(item.replace(matching[0], ""));
+            result.push(new UnityYamlData(classId, fileId, unityClass, matching[3] != undefined));
+        }
+    });
+    return result;
 }
 
-class UnityYamlData {
+export class UnityYamlData {
     classId: string;
     fileId: string;
     data: UnityClassType.UnityClass;
